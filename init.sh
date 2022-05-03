edb5aa595f4d1ce918f1b430a67b0bbad7c4cb4c
Christian J. Mills <millscj+github@protonmail.com>
2022-05-03 14:16:46 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
 
+packages="ansible"
+
+printf "\n\nPerforming initial setup..."
+
+printf "\n\nUpdating package cache...\n\n"
 sudo apt update
 sudo apt upgrade -y
-sudo apt install git ansible -y
+
+printf "\n\nInstalling packages...\n\n"
+sudo apt install $packages -y
+
+printf "\n\nUninstalling packages...\n\n"
+sudo apt remove $packages -y
+sudo apt autoremove -y
